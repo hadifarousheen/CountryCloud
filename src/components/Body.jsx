47c215a2a9cad9d1c6c074a2586cb0e78d0ae9c1
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-01-12 13:57:17 +0530

    implemented search feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CountryContext from "../utils/CountyContext";
 import CountryCard from "./CountryCard";
 
 const Body=()=>{
-    const {countryData}=useContext(CountryContext);
+    const {countryData,setCountryData,fixedCountryData}=useContext(CountryContext);
+    const[searchCountry,setSearchCountry]=useState("");
     return(
-        <div className=" h-screen flex flex-wrap gap-10 justify-center mt-6">
+        <div className="pt-6 bg-gray-300">
+        <div className="flex justify-center">
+            <input type="text" value={searchCountry} className="border w-1/3 px-1 font-black border-2" onChange={(e)=>{
+                setSearchCountry(e.target.value)
+            }}/>
+            <button className=" px-2 ml-2 rounded-lg font-bold shadow-md bg-amber-300" onClick={()=>{
+             const resultCountry=fixedCountryData?.filter((country)=>{return country.name.common.toLowerCase()==searchCountry.toLowerCase()});
+             setCountryData(resultCountry)
+            }}>Search</button>
+
+             <button className=" px-2 ml-2 rounded-lg font-bold shadow-md bg-amber-300" onClick={()=>{
+             setCountryData(fixedCountryData)
+            }}>Get All Countries</button>
+        </div>
+        <div className="  flex flex-wrap gap-10 justify-center mt-6">
             {countryData?.map((country)=>{
                return <CountryCard data={country} key={country?.flags?.png}/>
             })}
+        </div>
         </div>
     )
 }
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 const CountryCard=({data})=>{
     
     const{flags,name}=data;
     return(
-        <div className="">
+    <Link to="/">    <div className="">
            
                
                 <img src={flags?.png} className="h-60 w-96"/>
-                <h1>{name?.official}</h1>
+                <h1 className="font-bold text-lg">{name?.common}</h1>
             
         </div>
+        </Link>
     )
 }
 
